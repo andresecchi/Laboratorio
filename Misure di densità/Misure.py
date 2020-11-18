638cfb039bed4cad12d7a3e11c26968e812618e7
andresecchi <74241676+andresecchi@users.noreply.github.com>
2020-11-18 13:45:18 +0100

    Update Misure.py

--- Misure di densità/Misure.py
+++ Misure di densità/Misure.py
@@ -6,10 +6,10 @@ rriga = 0.1  # Risoluzione della riga [cm]
 # sigma_XX rappresenta l'incertezza della misura corrispondente
 
 tara_acqua = 350          # [g]
-d = 8.                                                      # Diametro del cilindro [cm]
+d = 8.                                                                # Diametro del cilindro [cm]
 sigma_d = rriga
 
-h_acqua = np.array([4.5, 5.7, 6.6, 7.4, 8.6])              # Altezza dell'acqua [cm]
+h_acqua = np.array([4.5, 5.7, 6.6, 7.4, 8.6])                         # Altezza dell'acqua [cm]
 sigma_h_acqua = np.full(h_acqua.shape, rriga)
 
 m_acquatara = np.array([352, 408, 454, 498, 558])
@@ -19,7 +19,7 @@ sigma_m_acqua = np.full(m_acqua.shape, np.sqrt(rbilancia**2 + rbilancia**2))
 
 ################
 tara_olio = 350
-h_olio = np.array([4.5, 5.4, 6.4, 7.2, 8.9 ])       # Altezza dell' olio [h]
+h_olio = np.array([4.5, 5.4, 6.4, 7.2, 8.9 ])                          # Altezza dell' olio [h]
 sigma_h_olio = np.full(h_olio.shape, rriga)
 
 m_oliotara = np.array([352., 390., 436., 496., 554. ])
