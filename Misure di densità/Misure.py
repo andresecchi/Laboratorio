a5f6c3c52196a63e2069dcf5d76f9b5eaf2ae703
andresecchi <74241676+andresecchi@users.noreply.github.com>
2020-11-18 13:42:24 +0100

    Add files via upload

---
+++ Misure di densità/Misure.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+rbilancia = 2  # Risoluzione della bilancia [g]
+rriga = 0.1  # Risoluzione della riga [cm]
+
+# sigma_XX rappresenta l'incertezza della misura corrispondente
+
+tara_acqua = 350          # [g]
+d = 8.                                                      # Diametro del cilindro [cm]
+sigma_d = rriga
+
+h_acqua = np.array([4.5, 5.7, 6.6, 7.4, 8.6])              # Altezza dell'acqua [cm]
+sigma_h_acqua = np.full(h_acqua.shape, rriga)
+
+m_acquatara = np.array([352, 408, 454, 498, 558])
+
+m_acqua = m_acquatara - np.full(m_acquatara.shape, tara_acqua)        # Massa dell'acqua [g]
+sigma_m_acqua = np.full(m_acqua.shape, np.sqrt(rbilancia**2 + rbilancia**2))
+
+################
+tara_olio = 350
+h_olio = np.array([4.5, 5.4, 6.4, 7.2, 8.9 ])       # Altezza dell' olio [h]
+sigma_h_olio = np.full(h_olio.shape, rriga)
+
+m_oliotara = np.array([352., 390., 436., 496., 554. ])
+
+m_olio = m_oliotara - np.full(m_oliotara.shape, tara_olio)            # Massa del' olio [g]
+sigma_m_olio = np.full(m_olio.shape, np.sqrt((rbilancia)**2 + (rbilancia)**2))
+
+
+
+
+
+
+
+
+
---
+++ Misure di densità/main.py
@@ -0,0 +1,52 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.optimize import curve_fit
+import Misure as me
+
+#  Definizione della funzione di fit
+
+
+def line(x, m, q):
+
+    return m * x + q
+
+#  Trovo il best-fit e la matrice di convarianza
+
+
+popt_acqua, pcov_acqua = curve_fit(line, me.m_acqua, me.h_acqua)
+mhat_acqua, qhat_acqua = popt_acqua
+sigma_mhat_acqua, sigma_qhat_acqua = np.sqrt(pcov_acqua.diagonal())
+
+popt_olio, pcov_olio = curve_fit(line, me.m_olio, me.h_olio)
+mhat_olio, qhat_olio = popt_olio
+sigma_mhat_olio, sigma_qhat_olio = np.sqrt(pcov_olio.diagonal())
+
+#  Disegno il grafico cone le misure sperimentali e il loro errore
+
+plt.figure('Grafico massa-altezza')
+plt.errorbar(me.m_acqua, me.h_acqua, me.sigma_h_acqua, me.sigma_m_acqua, fmt='.')
+plt.errorbar(me.m_olio, me.h_olio, me.sigma_h_olio, me.sigma_m_olio, fmt='.')
+plt.xlabel('Massa [g]')
+plt.ylabel('Altezza [cm]')
+plt.grid(ls='dashed')
+
+# Inserisco il grafico del fit
+
+x = np.linspace(0., 300, 100)
+plt.plot(x, line(x, mhat_acqua, qhat_acqua), color = "blue", label = 'Acqua')
+plt.plot(x, line(x, mhat_olio, qhat_olio), color = "red", label = 'Olio')
+
+
+D_acqua = 1/(np.pi * (me.d/2)**2 * mhat_acqua)
+sigma_D_acqua = sigma_mhat_acqua/ mhat_acqua + 2*(me.rriga /2 / (me.d/2) )
+
+D_olio = 1/(np.pi * (me.d/2)**2 * mhat_olio)
+sigma_D_olio = sigma_mhat_olio/ mhat_olio + 2*(me.rriga /2 / (me.d/2) )
+
+print(f'Densità acqua = {D_acqua} +/- {sigma_D_acqua}')
+print(f'Densità olio = {D_olio} +/- {sigma_D_olio}')
+
+
+
+
+plt.show()
